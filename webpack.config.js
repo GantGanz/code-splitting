29699006f24bf2d63457a8cd449e0774ac05672c
GantGanz <anggerelegant7@gmail.com>
2021-06-19 22:21:59 +0700

    code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
- 
+
 module.exports = {
-entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
 output: {
-  filename: 'bundle.js',
+  filename: '[name].bundle.js',
   path: path.resolve(__dirname, 'dist'),
 },
 module: {
@@ -30,4 +30,27 @@ plugins: [
   }),
   new BundleAnalyzerPlugin(),
 ],
-};
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+ };
